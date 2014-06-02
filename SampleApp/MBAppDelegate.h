aec889cff3f73e3a74406de0022756aa6c558216
markbr <mark.bryant@dat.com>
2014-06-02 16:51:27 -0700

    adding the skeleton project files

---
+++ SampleApp/MBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MBAppDelegate.h
+//  SampleApp
+//
+//  Created by Mark Bryant on 6/2/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SampleApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SampleApp
+//
+//  Created by Mark Bryant on 6/2/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MBAppDelegate class]));
+    }
+}
